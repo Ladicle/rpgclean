a78fbd22fb6ff383493554b7fe02f58c0bfcb2b9
Ladicle <Ladiclexxx@gmail.com>
2015-09-29 04:20:44 +0900

    Add demo code.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.Onload = function() {
+    var output = document.getElementById('output');
+    
+    window.addEventListener('devicemotion', function(e) {
+	gravity = e.accelerationIncludingGravity;
+	output.innerHTML = 'x:' + gravity.x + 'y:' + gravity.y;
+    }, true);
+};
